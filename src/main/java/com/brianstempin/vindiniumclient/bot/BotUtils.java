e695ba8851811a36f3e3bd9d9e7dfb7f80b9ae39
Vicken52 <krikorian.vicken@gmail.com>
2015-12-11 22:35:50 -0800

    Fixed Nullpointer

--- src/main/java/com/brianstempin/vindiniumclient/bot/BotUtils.java
+++ src/main/java/com/brianstempin/vindiniumclient/bot/BotUtils.java
@@ -70,7 +70,7 @@ public class BotUtils {
 
         for(GameState.Hero currentHero : gameState.getHeroesByPosition().values()) {
             GameState.Position currentHeroPosition = currentHero.getPos();
-            if(searchResults.get(currentHeroPosition).getDistance() <= radius
+            if(searchResults != null && searchResults.get(currentHeroPosition).getDistance() <= radius
                     && currentHero.getId() != gameState.getMe().getId())
                 heroes.add(currentHero);
         }
